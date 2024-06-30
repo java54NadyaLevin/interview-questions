7350294c88779257d988f8a6db732d49df2b57ce
Nadelele <nadele@gmail.com>
2024-06-30 13:08:09 +0300

    implemented collectionPool and 2 interview methods

--- src/telran/interviews/ConnectionPool.java
+++ src/telran/interviews/ConnectionPool.java
@@ -1,18 +1,32 @@
 package telran.interviews;
-//all methods should have O[1] complexity
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class ConnectionPool {
-	//TODO Data Structure
+
+	LinkedHashMap<Long, Integer> map;
+	@SuppressWarnings("serial")
 	public ConnectionPool(int size) {
-		//TODO
+		map = new 
+				LinkedHashMap<>(10, 0.75f, true) {
+			@Override
+			protected boolean removeEldestEntry(Map.Entry<Long, Integer> entry) {
+				return size() > size;
+			}
+		};
 	}
 public Connection getConnection(Connection connection) {
-	//TODO return a connection from the pool if it exists
-	//otherwise creates new connection, adds in pool and returns new created connection
-	return null;
+	boolean inPool = isInPool(connection.id());
+	Connection newConnection = null;
+	if(!inPool) {
+		newConnection = new Connection(connection.id());
+		map.put(newConnection.id(), Integer.valueOf(1));
+	}
+
+	return inPool ? connection : newConnection;
 }
 public boolean isInPool(long id) {
-	//TODO returns true if  a given connection exists in the pool
-	//otherwise returns false
-	return false;
+	return map.get(id) != null;
 }
 }
--- src/telran/interviews/InterviewQuestions.java
+++ src/telran/interviews/InterviewQuestions.java
@@ -1,45 +1,63 @@
 package telran.interviews;
+
 import java.util.*;
+
 public class InterviewQuestions {
-public static void displayOccurrences(String [] strings) {
-	HashMap<String, Integer> mapOccurrences = getOccurrencesMap(strings);
-	TreeMap<Integer, TreeSet<String>> treeMapOccurrences = getTreeMapOccurrences(mapOccurrences);
-	displayOccurrences(treeMapOccurrences);
-}
-
-private static void displayOccurrences(TreeMap<Integer, TreeSet<String>> treeMapOccurrences) {
-	treeMapOccurrences.entrySet().forEach(e -> {
-		e.getValue().forEach(str -> System.out.printf("%s => %d\n",str, e.getKey()));
-	});
-	
-}
-
-private static TreeMap<Integer, TreeSet<String>> getTreeMapOccurrences(HashMap<String, Integer> mapOccurrences) {
-	TreeMap<Integer, TreeSet<String>> result =
-			new TreeMap<Integer, TreeSet<String>>(Comparator.reverseOrder());
-	mapOccurrences.entrySet()
-	.forEach(e -> result.computeIfAbsent(e.getValue(), k -> new TreeSet<>() ).add(e.getKey()));
-	
-	return result;
-}
-
-private static HashMap<String, Integer> getOccurrencesMap(String[] strings) {
-	HashMap<String, Integer> result = new HashMap<>();
-	for(String str: strings) {
-		result.merge(str, 1, Integer::sum);
+	public static void displayOccurrences(String[] strings) {
+		HashMap<String, Integer> mapOccurrences = getOccurrencesMap(strings);
+		TreeMap<Integer, TreeSet<String>> treeMapOccurrences = getTreeMapOccurrences(mapOccurrences);
+		displayOccurrences(treeMapOccurrences);
 	}
-	return result;
-}
-static public boolean isSum2(int [] array, int sum) {
-	//TODO returns true if a given array contains two numbers, the summing of which
-	//equals a given 'sum' value
-	//complexity O[N] only one pass over the elements
-	return false;
-}
-static public int getMaxWithNegativePresentation(int [] array) {
-	//TODO returns maximal positive value for which exists negative one with the same abs value
-	//if no pair of positive and negative values with the same abs value the method returns -1
-	//complexity O[N] only one pass over the elements
-	return -1;
-}
+
+	private static void displayOccurrences(TreeMap<Integer, TreeSet<String>> treeMapOccurrences) {
+		treeMapOccurrences.entrySet().forEach(e -> {
+			e.getValue().forEach(str -> System.out.printf("%s => %d\n", str, e.getKey()));
+		});
+
+	}
+
+	private static TreeMap<Integer, TreeSet<String>> getTreeMapOccurrences(HashMap<String, Integer> mapOccurrences) {
+		TreeMap<Integer, TreeSet<String>> result = new TreeMap<Integer, TreeSet<String>>(Comparator.reverseOrder());
+		mapOccurrences.entrySet()
+				.forEach(e -> result.computeIfAbsent(e.getValue(), k -> new TreeSet<>()).add(e.getKey()));
+
+		return result;
+	}
+
+	private static HashMap<String, Integer> getOccurrencesMap(String[] strings) {
+		HashMap<String, Integer> result = new HashMap<>();
+		for (String str : strings) {
+			result.merge(str, 1, Integer::sum);
+		}
+		return result;
+	}
+
+	static public boolean isSum2(int[] array, int sum) {
+		Set<Integer> hashSet = hashSetSetup(array);
+		Iterator<Integer> it = hashSet.iterator();
+		boolean sumFound = false;
+		while (it.hasNext() && !sumFound) {
+			sumFound = hashSet.contains(sum - it.next());
+		}
+		return sumFound;
+	}
+
+	static public int getMaxWithNegativePresentation(int[] array) {
+		Set<Integer> hashSet = hashSetSetup(array);
+		int maxValue = -1;
+		for (int i : hashSet) {
+			if (hashSet.contains(-i) && Math.abs(i) > maxValue) {
+				maxValue = Math.abs(i);
+			}
+		}
+		return maxValue;
+	}
+
+	private static Set<Integer> hashSetSetup(int[] array) {
+		Set<Integer> hashSet = new HashSet<>();
+		for (Integer e : array) {
+			hashSet.add(e);
+		}
+		return hashSet;
+	}
 }
